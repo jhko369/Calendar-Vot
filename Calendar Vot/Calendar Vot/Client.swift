f034e255bce4dc3a391ffcc4f24f806b9366a68a
JwaJungwoo <jwajw93@gmail.com>
2016-11-11 09:43:15 +0900

    Add Project Folder
    .

---
+++ Calendar Vot/Calendar Vot/Client.swift
@@ -0,0 +1,90 @@
+//
+//  Client.swift
+//  Calendar Vot
+//
+//  Created by Neobono_Mac1 on 2016. 11. 11..
+//  Copyright © 2016년 Neobono_Mac1. All rights reserved.
+//
+
+//
+//  MakeOptionModule.swift
+//  Project_Jwa_Team
+//
+//  Created by Neobono_Mac1 on 2016. 11. 10..
+//  Copyright © 2016년 Neobono_Mac1. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class InputData : UITableViewController
+{
+    
+}
+
+class VoteName
+{
+    var name : String = ""
+    
+    init()
+    {
+        self.name = "투표"
+    }
+    
+    init(name : String)
+    {
+        self.name = name
+    }
+}
+
+class MeetingDate
+{
+    var startDate : NSDate!
+    var endDate : NSDate!
+    
+    init(startDate : NSDate, endDate : NSDate)
+    {
+        self.startDate = startDate
+        self.endDate = endDate
+    }
+}
+
+class MeetingPlace
+{
+    var placeName : String!
+    
+    init(placeName : String) {
+        self.placeName = placeName
+    }
+}
+
+class VoteOption
+{
+    
+}
+
+class Vote
+{
+    var name : VoteName
+    var date : [Int : MeetingDate]
+    var place : [Int : MeetingPlace]
+    
+    init(name : VoteName, date : [Int : MeetingDate], place : [Int : MeetingPlace])
+    {
+        self.name = name
+        self.date = date
+        self.place = place
+    }
+}
+
+var voteName : VoteName? = VoteName.init(name: "에러 없음")
+var meetingDate : [Int : MeetingDate] = [:]
+var meetingPlace : [Int : MeetingPlace] = [:]
+
+
+
+
+
+
+
+

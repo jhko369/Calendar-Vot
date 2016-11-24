077dd016b1ce7f83402cb678962b435553b41620
yhr7518 <owner@ownerui-MacBook-Air.local>
2016-11-24 14:21:30 +0900

    Input view에  date picker 를 넣어 시작 시간과 종료 시간을 입력받을 수 있도록 함.

--- Calendar Vot/Calendar Vot/DateCell.swift
+++ Calendar Vot/Calendar Vot/DateCell.swift
@@ -10,34 +10,86 @@ import UIKit
 
 class DateCell: UITableViewCell {
 
-
-    @IBOutlet weak var startField: UITextField!
+   
+    var startend:String = ""
+    var datePickerView:UIView = UIView()
+    var datePicker:UIDatePicker = UIDatePicker()
+    var doneBtn = UIButton()
     
+    
+    
+    @IBOutlet weak var startField: UITextField!
     @IBAction func startFieldEditing(_ sender: UITextField) {
         
-        let datePickerView:UIDatePicker = UIDatePicker()
-        
-        datePickerView.datePickerMode = UIDatePickerMode.dateAndTime
-        datePickerView.locale = NSLocale(localeIdentifier: "en_GB") as Locale
-        
+        startend = "start"
+        showDatePicker()
         sender.inputView = datePickerView
+        doneBtn.addTarget(self, action: #selector(DateCell.doneButton), for: UIControlEvents.touchUpInside)
+        datePicker.addTarget(self, action: #selector(DateCell.datePickerValueChanged), for: UIControlEvents.valueChanged)
+    }
+    
+    
+    @IBOutlet weak var endField: UITextField!
+    @IBAction func endFieldEditing(_ sender: UITextField) {
+    
+        startend = "end"
+        showDatePicker()
+        sender.inputView = datePickerView
+        doneBtn.addTarget(self, action: #selector(DateCell.doneButton), for: UIControlEvents.touchUpInside)
+        datePicker.addTarget(self, action: #selector(DateCell.datePickerValueChanged), for: UIControlEvents.valueChanged)
+
+    }
+    
+    
+    func showDatePicker(){
+    
+        datePickerView = UIView(frame: CGRect(x: 0, y: 0, width: self.frame.size.width, height:240))
+        datePicker = UIDatePicker(frame: CGRect(x: 0, y: 40, width: self.frame.size.width , height: 200))
+        datePicker.datePickerMode = UIDatePickerMode.dateAndTime
+        datePicker.minuteInterval = 5
+        datePickerView.addSubview(datePicker)
         
-        datePickerView.addTarget(self, action: #selector(DateCell.datePickerValueChanged), for: UIControlEvents.valueChanged)
-        
+        doneBtn = UIButton(frame: CGRect(x: self.frame.size.width - 100,y: 0, width: 100, height: 50))
+        doneBtn.setTitle("Done", for: UIControlState.normal)
+        doneBtn.setTitle("Done", for: UIControlState.highlighted)
+        doneBtn.setTitleColor(UIColor.black, for: UIControlState.normal)
+        doneBtn.setTitleColor(UIColor.gray, for: UIControlState.highlighted)
+        datePickerView.addSubview(doneBtn)
+
+
     }
     
     func datePickerValueChanged(sender:UIDatePicker) {
         
         let dateFormatter = DateFormatter()
-        
-        dateFormatter.dateStyle = DateFormatter.Style.medium
-        
-        dateFormatter.timeStyle = DateFormatter.Style.none
-        
-        startField.text = dateFormatter.string(from: sender.date)
+        dateFormatter.dateFormat = "yyyy.MM.dd HH:mm"
+
+        if startend == "start"
+        {
+            startField.text = dateFormatter.string(from: sender.date)
+        }
+        else
+        {
+            endField.text = dateFormatter.string(from: sender.date)
+        }
         
     }
     
+    
+    func doneButton(sender:UIButton)
+    {
+        if startend == "start"
+        {
+            startField.resignFirstResponder()
+        }
+        else
+        {
+            endField.resignFirstResponder()
+        }
+        
+
+
+    }
     
     override func awakeFromNib() {
         super.awakeFromNib()

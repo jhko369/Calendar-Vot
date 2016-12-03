edf2f0a7b265a27c63212d3657aa538f6d27fe7d
JwaJungwoo <jwajw93@gmail.com>
2016-12-03 21:45:16 +0900

    Modify for Test

--- Calendar Vot/Calendar Vot/AddViewController.swift
+++ Calendar Vot/Calendar Vot/AddViewController.swift
@@ -193,6 +193,11 @@ class AddViewController: UIViewController, UITableViewDelegate, UITableViewDataS
                     
                 }
             }
+            
+            if(section == 2)
+            {
+                
+            }
         }
     }
 }

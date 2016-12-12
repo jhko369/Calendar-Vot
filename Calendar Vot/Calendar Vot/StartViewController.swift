96ea13b7e7668bc047bc3d0bd85c3b031926b1ec
yhr7518 <yhr7518@gmail.com>
2016-12-13 03:38:08 +0900

    Start view 수

--- Calendar Vot/Calendar Vot/StartViewController.swift
+++ Calendar Vot/Calendar Vot/StartViewController.swift
@@ -13,14 +13,20 @@ protocol StartViewControllerDelegate: class
     func startViewControllerDidSelectAdd()
 }
 
+
+
 class StartViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
 {
     @IBOutlet weak var StartTable: UITableView!
     
     static let storyboardIdentifier = "StartView"
     weak var delegate: StartViewControllerDelegate?
+
+    var votes:[Vote] = []
+    var vote0:Vote!, vote1:Vote!, vote2:Vote!
+
     
-    let votes:[String] = ["아이디어 회의", "회식", "중간평가"]
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -30,6 +36,48 @@ class StartViewController: UIViewController, UITableViewDelegate, UITableViewDat
         
         print("Date  \(Date.init())")
         print("NS  \(NSDate.init())")
+        loadVoteData()
+    }
+    
+    func loadVoteData()
+    {
+        vote0 = Vote.init()
+        vote0.voteName = "아이디어 회의"
+        vote0.dateData = [(Date.init(), 0), (Date.init().addingTimeInterval(60*60*22),2), (Date.init().addingTimeInterval(60*60*42),1)]
+        vote0.locationData = [("카페워크샵", 3),("도서관 세미나룸", 0)]
+        vote0.multiSelect.option = "false"
+        vote0.addItem.option = "false"
+        vote0.finishSet.option = "false"
+        vote0.created.isCreated = "true"
+        vote0.DateDataSetting()
+        vote0.LocationDataSetting()
+       
+        vote1 = Vote.init()
+        vote1.voteName = "캡스톤 회식"
+        vote1.dateData = [(Date.init(), 0), (Date.init().addingTimeInterval(60*60*80),1), (Date.init().addingTimeInterval(60*60*100),5)]
+        vote1.locationData = [("족발", 2),("피자", 1),("치킨", 3)]
+        vote1.multiSelect.option = "false"
+        vote1.addItem.option = "false"
+        vote1.finishSet.option = "false"
+        vote1.created.isCreated = "true"
+        vote1.DateDataSetting()
+        vote1.LocationDataSetting()
+        
+        vote2 = Vote.init()
+        vote2.voteName = "발표자료 준비"
+        vote2.dateData = [ (Date.init().addingTimeInterval(60*60*30),2), (Date.init().addingTimeInterval(60*60*50),1)]
+        vote2.locationData = [("각자 집에서", 2),("실습실", 1),("충무로 카페", 0)]
+        vote2.multiSelect.option = "false"
+        vote2.addItem.option = "false"
+        vote2.finishSet.option = "false"
+        vote2.created.isCreated = "true"
+        vote2.DateDataSetting()
+        vote2.LocationDataSetting()
+        
+        votes.append(vote0)
+        votes.append(vote1)
+        votes.append(vote2)
+        
     }
     
     override func didReceiveMemoryWarning()
@@ -60,7 +108,7 @@ class StartViewController: UIViewController, UITableViewDelegate, UITableViewDat
         else
         {
             let cell = tableView.dequeueReusableCell(withIdentifier: "HistoryCell", for: indexPath)
-            cell.textLabel?.text = votes[indexPath.row]
+            cell.textLabel?.text = votes[indexPath.row].voteName
             
             return cell
         }
@@ -91,5 +139,19 @@ class StartViewController: UIViewController, UITableViewDelegate, UITableViewDat
             print("")
             delegate?.startViewControllerDidSelectAdd()
         }
+        else
+        {
+            
+        }
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "resultSegue"{
+            if let destination = segue.destination as? ResultViewController , let selectedIndex = StartTable.indexPathForSelectedRow?.row {
+                destination.voteData = votes[selectedIndex] as Vote
+            }
+        }
+        
     }
 }
